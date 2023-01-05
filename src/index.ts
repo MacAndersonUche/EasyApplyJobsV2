4fddb90a3badab97aee7275a95369abce834ba32
MacAndersonUche <MacAndersonUche@yahoo.com>
2023-01-05 01:19:00 +0000

    works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+// import { details } from "./env.config";
+import * as dotenv from "dotenv";
+import { details } from "./env.config";
 import { login } from "./util";
 
-login()
+
+dotenv.config({ path: __dirname + './env.config.ts' });
+
+
+
+console.log("Email Address: " + details.emailAddress);
 
--- src/util.ts
+++ src/util.ts
@@ -1,58 +1,30 @@
 import puppeteer from "puppeteer";
-// import { homeUrl } from "./constants";
-import * as dotenv from 'dotenv';
-import { details } from "./env.config";
+import { homeUrl } from "./constants";
 
 
-dotenv.config();
 
-
-// const login = async () => {
-//     const browser = await puppeteer.launch({ headless: false })
-//     const page = await browser.newPage()
-
-//     await page.setViewport({ width: 1280, height: 800 })
-//     await page.goto(homeUrl);
-
-//     await page.waitForSelector('#username');
-//     await page.type('#username', details.emailAddress);
-
-//     // Wait for the password field to be displayed and enter the password
-//     await page.waitForSelector('#password');
-//     await page.type('#password', details.passWord);
-
-//     // Click the login button
-//     await page.click('button[type="submit"]');
-
-//     // // Wait for the profile page to load and get the logged in user's name
-//     // await page.waitForSelector('#profile-nav-item');
-//     // const name = await page.$eval('#profile-nav-item', (el) => el.innerText);
-
-//     console.log(`Logged in`)
-// }
-
-async function login() {
+async function login(email: string, password: string) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto('https://www.linkedin.com/uas/login');
+    await page.goto(homeUrl);
 
     // Wait for the email field to be displayed and enter the email
     await page.waitForSelector('#username');
-    await page.type('#username', details.emailAddress);
+    await page.type('#username', email);
 
     // Wait for the password field to be displayed and enter the password
     await page.waitForSelector('#password');
-    await page.type('#password', details.passWord);
+    await page.type('#password', password);
 
     // Click the login button
     await page.click('button[type="submit"]');
 
     // // Wait for the profile page to load and get the logged in user's name
-    // await page.waitForSelector('#profile-nav-item');
+    await page.waitForSelector('#profile-nav-item');
     // const name = await page.$eval('#profile-nav-item', (el) => el.innerText);
 
-    // console.log(`Logged in as ${name}`);
+    console.log(`Logged in as ${name}`);
 }
 
 export { login };
